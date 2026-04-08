1e6a654ab8f465954dec935a27ba0d83e5c462c2
SakshamNarvar <sakshamnarvar449@gmail.com>
2026-04-08 16:06:48 +0530

    llm sends back fund_source

--- DsService/src/app/service/Expense.py
+++ DsService/src/app/service/Expense.py
@@ -6,10 +6,12 @@ class Expense(BaseModel):
         amount: Optional[str] = Field(title="expense", description="Expense made on the transaction")
         merchant: Optional[str] = Field(title="merchant", description="Merchant name to whom the transaction has been made")
         currency: Optional[str] = Field(title="currency", description="Currency of the transaction (only allowed values = INR, USD, otherwise null)")
+        fund_source: Optional[str] = Field(title="fund_source", description="Fund source of the transaction like SBI Card, ICICI Bank, otherwise null)")
 
         def serialize(self):
                 return {
                         "amount": self.amount,
                         "merchant": self.merchant,
-                        "currency": self.currency
+                        "currency": self.currency,
+                        "fund_source": self.fund_source
                 }
