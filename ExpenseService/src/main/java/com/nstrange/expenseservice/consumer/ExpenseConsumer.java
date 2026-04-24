6e71271afa731b8f8bd54c1ae0b844cb5c12f524
SakshamNarvar <sakshamnarvar449@gmail.com>
2026-04-24 16:27:51 +0530

    fixed serialization bug

--- ExpenseService/src/main/java/com/nstrange/expenseservice/consumer/ExpenseConsumer.java
+++ ExpenseService/src/main/java/com/nstrange/expenseservice/consumer/ExpenseConsumer.java
@@ -5,6 +5,7 @@ import com.nstrange.expenseservice.service.ExpenseService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,12 @@ public class ExpenseConsumer
 
     private static final Logger log = LoggerFactory.getLogger(ExpenseConsumer.class);
 
-    private final ExpenseService expenseService;
+    private ExpenseService expenseService;
+
+    @Autowired
+    ExpenseConsumer(ExpenseService expenseService){
+        this.expenseService = expenseService;
+    }
 
     @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(ExpenseDto eventData) {
